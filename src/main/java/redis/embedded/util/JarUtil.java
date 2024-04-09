c5e7dfb71052758796881950b2a092b4e12c7df8
徐长顺 <xucs@hjsoft.com.cn>
2024-04-09 16:07:05 +0800

    To resolve the issue of the service failing to start due to the nonexistence of the temporary directory,

--- src/main/java/redis/embedded/util/JarUtil.java
+++ src/main/java/redis/embedded/util/JarUtil.java
@@ -10,6 +10,11 @@ import java.io.IOException;
 public class JarUtil {
 
     public static File extractExecutableFromJar(String executable) throws IOException {
+        // To begin with, you need to check if the temporary directory exists. If it doesn't exist, create the temporary directory first.
+        File baseDir = new File(System.getProperty("java.io.tmpdir"));
+        if (!baseDir.exists()) {
+            baseDir.mkdirs();
+        }
         File tmpDir = Files.createTempDir();
         tmpDir.deleteOnExit();
 
